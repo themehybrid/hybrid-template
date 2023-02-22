61084eef1234e05ed5a972c389f1656a124084b2
saasfreelancer <syed.sayal.shah@gmail.com>
2023-02-22 13:36:48 +0500

    chore: update author, copyright

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ Hybrid Template is a set of helper functions for setting the location, locating,
 
 This project is licensed under the [GNU GPL](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html), version 2 or later.
 
-2008&thinsp;&ndash;&thinsp;2021 &copy; [Justin Tadlock](https://themehybrid.com).
+2008&thinsp;&ndash;&thinsp;2023 &copy; [Theme Hybrid](https://themehybrid.com).
--- src/functions-helpers.php
+++ src/functions-helpers.php
@@ -4,11 +4,11 @@
  *
  * Helper functions and template tags related to templates.
  *
- * @package   HybridCore
- * @link      https://themehybrid.com/hybrid-core
+ * @package   HybridTemplate
+ * @link      https://github.com/themehybrid/hybrid-template
  *
- * @author    Justin Tadlock <justintadlock@gmail.com>
- * @copyright Copyright (c) 2008 - 2021, Justin Tadlock
+ * @author    Theme Hybrid
+ * @copyright Copyright (c) 2008 - 2023, Theme Hybrid
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
